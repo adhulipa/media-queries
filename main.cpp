ae8db5424ad5d380725f634e4b244fe8313c73dc
Aditya Dhulipala <adhulipa@usc.edu>
2015-05-17 15:54:21 -0700

    Initial commit - test opencv pass!

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "MyImage.h"
+#include <iostream>
+
+cv::Mat src; cv::Mat dst;
+char window_name1[] = "Unprocessed Image";
+char window_name2[] = "Processed Image";
+
+void testMyImage();
+
+int main( int argc, char** argv )
+{
+    /// Load the source image
+    src = cv::imread( "../../Resources/fruits.jpg", 1 );
+
+    testMyImage();
+
+    //cv::namedWindow( window_name1, cv::WINDOW_AUTOSIZE );
+    //imshow("Unprocessed Image",src);
+
+    dst = src.clone();
+    GaussianBlur( src, dst, cv::Size( 15, 15 ), 0, 0 );
+
+    //cv::namedWindow( window_name2, cv::WINDOW_AUTOSIZE );
+    //imshow("Processed Image",dst);
+
+    cv::waitKey();
+    return 0;
+}
+
+void testMyImage() {
+    MyImage image;
+    image.setHeight(288);
+    image.setWidth(352);
+    image.setImagePath("../../Resources/flowers006.rgb");
+    image.ReadImage();
+
+    char *img;
+    img = image.getImageData();
+
+    std::cout << "Hello from testMyImage()" << std::endl;
+    std::cout << img[0] << std::endl;
+
+    cv::Mat TempMat = cv::Mat(288, 352, CV_8UC3, img);
+    imshow("this is a test",TempMat);
+
+}
